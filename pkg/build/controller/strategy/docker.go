openshift/origin

replace build context setup with init containers

pkg/build/controller/strategy/docker.go
@@ -11,7 +11,10 @@ import (
 
 // DockerBuildStrategy creates a Docker build using a Docker builder image.
 type DockerBuildStrategy struct {
-	Image string
+	Image                    string
+	GitCloneImage            string
+	ManageDockerfileImage    string
+	ExtractImageContentImage string
 	// Codec is the codec to use for encoding the output pod.
 	// IMPORTANT: This may break backwards compatibility when
 	// it changes.
@@ -48,6 +51,22 @@ func (bs *DockerBuildStrategy) CreateBuildPod(build *buildapi.Build) (*kapi.Pod,
 		},
 		Spec: kapi.PodSpec{
 			ServiceAccountName: build.Spec.ServiceAccount,
+			InitContainers: []kapi.Container{
+				{
+					Name:  "manage-dockerfile",
+					Image: bs.ManageDockerfileImage,
+					Env:   containerEnv,
+					Args:  []string{},
+					VolumeMounts: []kapi.VolumeMount{
+						{
+							Name:      "gitsource",
+							MountPath: "/tmp/gitSource",
+						},
+					},
+					ImagePullPolicy: kapi.PullIfNotPresent,
+					Resources:       build.Spec.Resources,
+				},
+			},
 			Containers: []kapi.Container{
 				{
 					Name:  "docker-build",
@@ -58,21 +77,77 @@ func (bs *DockerBuildStrategy) CreateBuildPod(build *buildapi.Build) (*kapi.Pod,
 					SecurityContext: &kapi.SecurityContext{
 						Privileged: &privileged,
 					},
+					VolumeMounts: []kapi.VolumeMount{
+						{
+							Name:      "gitsource",
+							MountPath: "/tmp/gitSource",
+						},
+					},
+					ImagePullPolicy: kapi.PullIfNotPresent,
+					Resources:       build.Spec.Resources,
+				},
+			},
+			Volumes: []kapi.Volume{
+				{
+					Name: "gitsource",
+					VolumeSource: kapi.VolumeSource{
+						EmptyDir: &kapi.EmptyDirVolumeSource{},
+					},
 				},
 			},
 			RestartPolicy: kapi.RestartPolicyNever,
 			NodeSelector:  build.Spec.NodeSelector,
 		},
 	}
-	pod.Spec.Containers[0].ImagePullPolicy = kapi.PullIfNotPresent
-	pod.Spec.Containers[0].Resources = build.Spec.Resources
+
+	// can't conditionalize the manage-dockerfile init container because we don't
+	// know until we've cloned, whether or not we've got a dockerfile to manage
+	// (also if it's a docker type build, we should always have a dockerfile to manage)
+	if build.Spec.Source.Git != nil || build.Spec.Source.Binary != nil {
+		gitCloneContainer := kapi.Container{
+			Name:  "git-clone",
+			Image: bs.GitCloneImage,
+			Env:   containerEnv,
+			Args:  []string{},
+			VolumeMounts: []kapi.VolumeMount{
+				{
+					Name:      "gitsource",
+					MountPath: "/tmp/gitSource",
+				},
+			},
+			ImagePullPolicy: kapi.PullIfNotPresent,
+			Resources:       build.Spec.Resources,
+		}
+		if build.Spec.Source.Binary != nil {
+			gitCloneContainer.Stdin = true
+			gitCloneContainer.StdinOnce = true
+		}
+		pod.Spec.InitContainers = append(pod.Spec.InitContainers, gitCloneContainer)
+	}
+	if len(build.Spec.Source.Images) > 0 {
+		extractImageContentContainer := kapi.Container{
+			Name:  "extract-image-content",
+			Image: bs.ExtractImageContentImage,
+			Env:   containerEnv,
+			Args:  []string{},
+			// TODO: run unprivileged https://github.com/openshift/origin/issues/662
+			SecurityContext: &kapi.SecurityContext{
+				Privileged: &privileged,
+			},
+			VolumeMounts: []kapi.VolumeMount{
+				{
+					Name:      "gitsource",
+					MountPath: "/tmp/gitSource",
+				},
+			},
+			ImagePullPolicy: kapi.PullIfNotPresent,
+			Resources:       build.Spec.Resources,
+		}
+		pod.Spec.InitContainers = append(pod.Spec.InitContainers, extractImageContentContainer)
+	}
 
 	if build.Spec.CompletionDeadlineSeconds != nil {
 		pod.Spec.ActiveDeadlineSeconds = build.Spec.CompletionDeadlineSeconds
-	}
-	if build.Spec.Source.Binary != nil {
-		pod.Spec.Containers[0].Stdin = true
-		pod.Spec.Containers[0].StdinOnce = true
 	}
 
 	setupDockerSocket(pod)

pkg/build/controller/strategy/sti.go
@@ -15,7 +15,9 @@ import (
 
 // SourceBuildStrategy creates STI(source to image) builds
 type SourceBuildStrategy struct {
-	Image string
+	Image                    string
+	GitCloneImage            string
+	ExtractImageContentImage string
 	// Codec is the codec to use for encoding the output pod.
 	// IMPORTANT: This may break backwards compatibility when
 	// it changes.
@@ -76,26 +78,79 @@ func (bs *SourceBuildStrategy) CreateBuildPod(build *buildapi.Build) (*kapi.Pod,
 					Name:  "sti-build",
 					Image: bs.Image,
 					Env:   containerEnv,
+					Args:  []string{},
 					// TODO: run unprivileged https://github.com/openshift/origin/issues/662
 					SecurityContext: &kapi.SecurityContext{
 						Privileged: &privileged,
 					},
-					Args: []string{},
+					VolumeMounts: []kapi.VolumeMount{
+						{
+							Name:      "gitsource",
+							MountPath: "/tmp/gitSource",
+						},
+					},
+					ImagePullPolicy: kapi.PullIfNotPresent,
+					Resources:       build.Spec.Resources,
+				},
+			},
+			Volumes: []kapi.Volume{
+				{
+					Name: "gitsource",
+					VolumeSource: kapi.VolumeSource{
+						EmptyDir: &kapi.EmptyDirVolumeSource{},
+					},
 				},
 			},
 			RestartPolicy: kapi.RestartPolicyNever,
 			NodeSelector:  build.Spec.NodeSelector,
 		},
 	}
-	pod.Spec.Containers[0].ImagePullPolicy = kapi.PullIfNotPresent
-	pod.Spec.Containers[0].Resources = build.Spec.Resources
+
+	if build.Spec.Source.Git != nil || build.Spec.Source.Binary != nil {
+		gitCloneContainer := kapi.Container{
+			Name:  "git-clone",
+			Image: bs.GitCloneImage,
+			Env:   containerEnv,
+			Args:  []string{},
+			VolumeMounts: []kapi.VolumeMount{
+				{
+					Name:      "gitsource",
+					MountPath: "/tmp/gitSource",
+				},
+			},
+			ImagePullPolicy: kapi.PullIfNotPresent,
+			Resources:       build.Spec.Resources,
+		}
+		if build.Spec.Source.Binary != nil {
+			gitCloneContainer.Stdin = true
+			gitCloneContainer.StdinOnce = true
+		}
+		pod.Spec.InitContainers = append(pod.Spec.InitContainers, gitCloneContainer)
+	}
+	if len(build.Spec.Source.Images) > 0 {
+		extractImageContentContainer := kapi.Container{
+			Name:  "extract-image-content",
+			Image: bs.ExtractImageContentImage,
+			Env:   containerEnv,
+			Args:  []string{},
+			// TODO: run unprivileged https://github.com/openshift/origin/issues/662
+			SecurityContext: &kapi.SecurityContext{
+				Privileged: &privileged,
+			},
+			VolumeMounts: []kapi.VolumeMount{
+				{
+					Name:      "gitsource",
+					MountPath: "/tmp/gitSource",
+				},
+			},
+			ImagePullPolicy: kapi.PullIfNotPresent,
+			Resources:       build.Spec.Resources,
+		}
+		pod.Spec.InitContainers = append(pod.Spec.InitContainers, extractImageContentContainer)
+	}
 
 	if build.Spec.CompletionDeadlineSeconds != nil {
 		pod.Spec.ActiveDeadlineSeconds = build.Spec.CompletionDeadlineSeconds
-	}
-	if build.Spec.Source.Binary != nil {
-		pod.Spec.Containers[0].Stdin = true
-		pod.Spec.Containers[0].StdinOnce = true
 	}
 
 	setupDockerSocket(pod)

pkg/build/controller/strategy/util.go
@@ -45,7 +45,7 @@ func IsFatal(err error) bool {
 }
 
 // setupDockerSocket configures the pod to support the host's Docker socket
-func setupDockerSocket(podSpec *kapi.Pod) {
+func setupDockerSocket(pod *kapi.Pod) {
 	dockerSocketVolume := kapi.Volume{
 		Name: "docker-socket",
 		VolumeSource: kapi.VolumeSource{
@@ -60,11 +60,17 @@ func setupDockerSocket(podSpec *kapi.Pod) {
 		MountPath: dockerSocketPath,
 	}
 
-	podSpec.Spec.Volumes = append(podSpec.Spec.Volumes,
+	pod.Spec.Volumes = append(pod.Spec.Volumes,
 		dockerSocketVolume)
-	podSpec.Spec.Containers[0].VolumeMounts =
-		append(podSpec.Spec.Containers[0].VolumeMounts,
+	pod.Spec.Containers[0].VolumeMounts =
+		append(pod.Spec.Containers[0].VolumeMounts,
 			dockerSocketVolumeMount)
+	for i, initContainer := range pod.Spec.InitContainers {
+		if initContainer.Name == "extract-image-content" {
+			pod.Spec.InitContainers[i].VolumeMounts = append(pod.Spec.InitContainers[i].VolumeMounts, dockerSocketVolumeMount)
+			break
+		}
+	}
 }
 
 // mountSecretVolume is a helper method responsible for actual mounting secret

openshift/openshift-controller-manager

pkg/build/controller/strategy/docker.go
@@ -8,7 +8,7 @@ import (
 	"k8s.io/kubernetes/pkg/api/v1"
 
 	buildapi "github.com/openshift/origin/pkg/build/apis/build"
-	"github.com/openshift/origin/pkg/build/util"
+	buildutil "github.com/openshift/origin/pkg/build/util"
 )
 
 // DockerBuildStrategy creates a Docker build using a Docker builder image.
@@ -39,7 +39,7 @@ func (bs *DockerBuildStrategy) CreateBuildPod(build *buildapi.Build) (*v1.Pod, e
 	addOriginVersionVar(&containerEnv)
 
 	if len(strategy.Env) > 0 {
-		util.MergeTrustedEnvWithoutDuplicates(util.CopyApiEnvVarToV1EnvVar(strategy.Env), &containerEnv, true)
+		buildutil.MergeTrustedEnvWithoutDuplicates(buildutil.CopyApiEnvVarToV1EnvVar(strategy.Env), &containerEnv, true)
 	}
 
 	pod := &v1.Pod{
@@ -52,30 +52,105 @@ func (bs *DockerBuildStrategy) CreateBuildPod(build *buildapi.Build) (*v1.Pod, e
 			ServiceAccountName: build.Spec.ServiceAccount,
 			Containers: []v1.Container{
 				{
-					Name:  "docker-build",
-					Image: bs.Image,
-					Env:   containerEnv,
-					Args:  []string{},
+					Name:    "docker-build",
+					Image:   bs.Image,
+					Command: []string{"openshift-docker-build"},
+					Env:     containerEnv,
 					// TODO: run unprivileged https://github.com/openshift/origin/issues/662
 					SecurityContext: &v1.SecurityContext{
 						Privileged: &privileged,
 					},
 					TerminationMessagePolicy: v1.TerminationMessageFallbackToLogsOnError,
+					VolumeMounts: []v1.VolumeMount{
+						{
+							Name:      "gitsource",
+							MountPath: buildutil.InputContentPath,
+						},
+					},
+					ImagePullPolicy: v1.PullIfNotPresent,
+					Resources:       buildutil.CopyApiResourcesToV1Resources(&build.Spec.Resources),
+				},
+			},
+			Volumes: []v1.Volume{
+				{
+					Name: "gitsource",
+					VolumeSource: v1.VolumeSource{
+						EmptyDir: &v1.EmptyDirVolumeSource{},
+					},
 				},
 			},
 			RestartPolicy: v1.RestartPolicyNever,
 			NodeSelector:  build.Spec.NodeSelector,
 		},
 	}
-	pod.Spec.Containers[0].ImagePullPolicy = v1.PullIfNotPresent
-	pod.Spec.Containers[0].Resources = util.CopyApiResourcesToV1Resources(&build.Spec.Resources)
+
+	// can't conditionalize the manage-dockerfile init container because we don't
+	// know until we've cloned, whether or not we've got a dockerfile to manage
+	// (also if it's a docker type build, we should always have a dockerfile to manage)
+	if build.Spec.Source.Git != nil || build.Spec.Source.Binary != nil {
+		gitCloneContainer := v1.Container{
+			Name:    "git-clone",
+			Image:   bs.Image,
+			Command: []string{"openshift-git-clone"},
+			Env:     containerEnv,
+			TerminationMessagePolicy: v1.TerminationMessageFallbackToLogsOnError,
+			VolumeMounts: []v1.VolumeMount{
+				{
+					Name:      "gitsource",
+					MountPath: buildutil.InputContentPath,
+				},
+			},
+			ImagePullPolicy: v1.PullIfNotPresent,
+			Resources:       buildutil.CopyApiResourcesToV1Resources(&build.Spec.Resources),
+		}
+		if build.Spec.Source.Binary != nil {
+			gitCloneContainer.Stdin = true
+			gitCloneContainer.StdinOnce = true
+		}
+		pod.Spec.InitContainers = append(pod.Spec.InitContainers, gitCloneContainer)
+	}
+	if len(build.Spec.Source.Images) > 0 {
+		extractImageContentContainer := v1.Container{
+			Name:    "extract-image-content",
+			Image:   bs.Image,
+			Command: []string{"openshift-extract-image-content"},
+			Env:     containerEnv,
+			// TODO: run unprivileged https://github.com/openshift/origin/issues/662
+			SecurityContext: &v1.SecurityContext{
+				Privileged: &privileged,
+			},
+			TerminationMessagePolicy: v1.TerminationMessageFallbackToLogsOnError,
+			VolumeMounts: []v1.VolumeMount{
+				{
+					Name:      "gitsource",
+					MountPath: buildutil.InputContentPath,
+				},
+			},
+			ImagePullPolicy: v1.PullIfNotPresent,
+			Resources:       buildutil.CopyApiResourcesToV1Resources(&build.Spec.Resources),
+		}
+		pod.Spec.InitContainers = append(pod.Spec.InitContainers, extractImageContentContainer)
+	}
+	pod.Spec.InitContainers = append(pod.Spec.InitContainers,
+		v1.Container{
+			Name:    "manage-dockerfile",
+			Image:   bs.Image,
+			Command: []string{"openshift-manage-dockerfile"},
+			Env:     containerEnv,
+			TerminationMessagePolicy: v1.TerminationMessageFallbackToLogsOnError,
+			VolumeMounts: []v1.VolumeMount{
+				{
+					Name:      "gitsource",
+					MountPath: buildutil.InputContentPath,
+				},
+			},
+			ImagePullPolicy: v1.PullIfNotPresent,
+			Resources:       buildutil.CopyApiResourcesToV1Resources(&build.Spec.Resources),
+		},
+	)
 
 	if build.Spec.CompletionDeadlineSeconds != nil {
 		pod.Spec.ActiveDeadlineSeconds = build.Spec.CompletionDeadlineSeconds
-	}
-	if build.Spec.Source.Binary != nil {
-		pod.Spec.Containers[0].Stdin = true
-		pod.Spec.Containers[0].StdinOnce = true
 	}
 
 	setOwnerReference(pod, build)

pkg/build/controller/strategy/sti.go
@@ -13,7 +13,8 @@ import (
 	"k8s.io/kubernetes/pkg/serviceaccount"
 
 	buildapi "github.com/openshift/origin/pkg/build/apis/build"
-	"github.com/openshift/origin/pkg/build/util"
+
+	buildutil "github.com/openshift/origin/pkg/build/util"
 )
 
 // SourceBuildStrategy creates STI(source to image) builds
@@ -51,7 +52,7 @@ func (bs *SourceBuildStrategy) CreateBuildPod(build *buildapi.Build) (*v1.Pod, e
 
 	strategy := build.Spec.Strategy.SourceStrategy
 	if len(strategy.Env) > 0 {
-		util.MergeTrustedEnvWithoutDuplicates(util.CopyApiEnvVarToV1EnvVar(strategy.Env), &containerEnv, true)
+		buildutil.MergeTrustedEnvWithoutDuplicates(buildutil.CopyApiEnvVarToV1EnvVar(strategy.Env), &containerEnv, true)
 	}
 
 	// check if can run container as root
@@ -75,30 +76,102 @@ func (bs *SourceBuildStrategy) CreateBuildPod(build *buildapi.Build) (*v1.Pod, e
 			ServiceAccountName: build.Spec.ServiceAccount,
 			Containers: []v1.Container{
 				{
-					Name:  "sti-build",
-					Image: bs.Image,
-					Env:   containerEnv,
+					Name:    "sti-build",
+					Image:   bs.Image,
+					Command: []string{"openshift-sti-build"},
+					Env:     containerEnv,
 					// TODO: run unprivileged https://github.com/openshift/origin/issues/662
 					SecurityContext: &v1.SecurityContext{
 						Privileged: &privileged,
 					},
-					Args: []string{},
 					TerminationMessagePolicy: v1.TerminationMessageFallbackToLogsOnError,
+					VolumeMounts: []v1.VolumeMount{
+						{
+							Name:      "gitsource",
+							MountPath: buildutil.InputContentPath,
+						},
+					},
+					ImagePullPolicy: v1.PullIfNotPresent,
+					Resources:       buildutil.CopyApiResourcesToV1Resources(&build.Spec.Resources),
+				},
+			},
+			Volumes: []v1.Volume{
+				{
+					Name: "gitsource",
+					VolumeSource: v1.VolumeSource{
+						EmptyDir: &v1.EmptyDirVolumeSource{},
+					},
 				},
 			},
 			RestartPolicy: v1.RestartPolicyNever,
 			NodeSelector:  build.Spec.NodeSelector,
 		},
 	}
-	pod.Spec.Containers[0].ImagePullPolicy = v1.PullIfNotPresent
-	pod.Spec.Containers[0].Resources = util.CopyApiResourcesToV1Resources(&build.Spec.Resources)
+
+	if build.Spec.Source.Git != nil || build.Spec.Source.Binary != nil {
+		gitCloneContainer := v1.Container{
+			Name:    "git-clone",
+			Image:   bs.Image,
+			Command: []string{"openshift-git-clone"},
+			Env:     containerEnv,
+			TerminationMessagePolicy: v1.TerminationMessageFallbackToLogsOnError,
+			VolumeMounts: []v1.VolumeMount{
+				{
+					Name:      "gitsource",
+					MountPath: buildutil.InputContentPath,
+				},
+			},
+			ImagePullPolicy: v1.PullIfNotPresent,
+			Resources:       buildutil.CopyApiResourcesToV1Resources(&build.Spec.Resources),
+		}
+		if build.Spec.Source.Binary != nil {
+			gitCloneContainer.Stdin = true
+			gitCloneContainer.StdinOnce = true
+		}
+		pod.Spec.InitContainers = append(pod.Spec.InitContainers, gitCloneContainer)
+	}
+	if len(build.Spec.Source.Images) > 0 {
+		extractImageContentContainer := v1.Container{
+			Name:    "extract-image-content",
+			Image:   bs.Image,
+			Command: []string{"openshift-extract-image-content"},
+			Env:     containerEnv,
+			// TODO: run unprivileged https://github.com/openshift/origin/issues/662
+			SecurityContext: &v1.SecurityContext{
+				Privileged: &privileged,
+			},
+			TerminationMessagePolicy: v1.TerminationMessageFallbackToLogsOnError,
+			VolumeMounts: []v1.VolumeMount{
+				{
+					Name:      "gitsource",
+					MountPath: buildutil.InputContentPath,
+				},
+			},
+			ImagePullPolicy: v1.PullIfNotPresent,
+			Resources:       buildutil.CopyApiResourcesToV1Resources(&build.Spec.Resources),
+		}
+		pod.Spec.InitContainers = append(pod.Spec.InitContainers, extractImageContentContainer)
+	}
+	pod.Spec.InitContainers = append(pod.Spec.InitContainers,
+		v1.Container{
+			Name:    "manage-dockerfile",
+			Image:   bs.Image,
+			Command: []string{"openshift-manage-dockerfile"},
+			Env:     containerEnv,
+			TerminationMessagePolicy: v1.TerminationMessageFallbackToLogsOnError,
+			VolumeMounts: []v1.VolumeMount{
+				{
+					Name:      "gitsource",
+					MountPath: buildutil.InputContentPath,
+				},
+			},
+			ImagePullPolicy: v1.PullIfNotPresent,
+			Resources:       buildutil.CopyApiResourcesToV1Resources(&build.Spec.Resources),
+		},
+	)
 
 	if build.Spec.CompletionDeadlineSeconds != nil {
 		pod.Spec.ActiveDeadlineSeconds = build.Spec.CompletionDeadlineSeconds
-	}
-	if build.Spec.Source.Binary != nil {
-		pod.Spec.Containers[0].Stdin = true
-		pod.Spec.Containers[0].StdinOnce = true
 	}
 
 	setOwnerReference(pod, build)

pkg/build/controller/strategy/util.go
@@ -21,12 +21,13 @@ import (
 
 const (
 	// dockerSocketPath is the default path for the Docker socket inside the builder container
-	dockerSocketPath               = "/var/run/docker.sock"
+	dockerSocketPath      = "/var/run/docker.sock"
+	sourceSecretMountPath = "/var/run/secrets/openshift.io/source"
+
 	DockerPushSecretMountPath      = "/var/run/secrets/openshift.io/push"
 	DockerPullSecretMountPath      = "/var/run/secrets/openshift.io/pull"
 	SecretBuildSourceBaseMountPath = "/var/run/secrets/openshift.io/build"
 	SourceImagePullSecretMountPath = "/var/run/secrets/openshift.io/source-image"
-	sourceSecretMountPath          = "/var/run/secrets/openshift.io/source"
 )
 
 var (
@@ -53,7 +54,7 @@ func IsFatal(err error) bool {
 }
 
 // setupDockerSocket configures the pod to support the host's Docker socket
-func setupDockerSocket(podSpec *v1.Pod) {
+func setupDockerSocket(pod *v1.Pod) {
 	dockerSocketVolume := v1.Volume{
 		Name: "docker-socket",
 		VolumeSource: v1.VolumeSource{
@@ -68,11 +69,17 @@ func setupDockerSocket(podSpec *v1.Pod) {
 		MountPath: dockerSocketPath,
 	}
 
-	podSpec.Spec.Volumes = append(podSpec.Spec.Volumes,
+	pod.Spec.Volumes = append(pod.Spec.Volumes,
 		dockerSocketVolume)
-	podSpec.Spec.Containers[0].VolumeMounts =
-		append(podSpec.Spec.Containers[0].VolumeMounts,
+	pod.Spec.Containers[0].VolumeMounts =
+		append(pod.Spec.Containers[0].VolumeMounts,
 			dockerSocketVolumeMount)
+	for i, initContainer := range pod.Spec.InitContainers {
+		if initContainer.Name == "extract-image-content" {
+			pod.Spec.InitContainers[i].VolumeMounts = append(pod.Spec.InitContainers[i].VolumeMounts, dockerSocketVolumeMount)
+			break
+		}
+	}
 }
 
 // mountSecretVolume is a helper method responsible for actual mounting secret