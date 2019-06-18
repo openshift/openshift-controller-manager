openshift/origin

work on build helpers

pkg/build/controller/strategy/custom_test.go
@@ -15,8 +15,7 @@ import (
 	kapihelper "k8s.io/kubernetes/pkg/apis/core/helper"
 
 	buildv1 "github.com/openshift/api/build/v1"
-	"github.com/openshift/origin/pkg/build/buildapihelpers"
-	"github.com/openshift/origin/pkg/build/util"
+	"github.com/openshift/origin/pkg/build/buildutil"
 )
 
 func TestCustomCreateBuildPod(t *testing.T) {
@@ -37,10 +36,10 @@ func TestCustomCreateBuildPod(t *testing.T) {
 		t.Fatalf("Unexpected error: %v", err)
 	}
 
-	if expected, actual := buildapihelpers.GetBuildPodName(build), actual.ObjectMeta.Name; expected != actual {
+	if expected, actual := buildutil.GetBuildPodName(build), actual.ObjectMeta.Name; expected != actual {
 		t.Errorf("Expected %s, but got %s!", expected, actual)
 	}
-	if !reflect.DeepEqual(map[string]string{util.BuildLabel: buildapihelpers.LabelValue(build.Name)}, actual.Labels) {
+	if !reflect.DeepEqual(map[string]string{buildutil.BuildLabel: buildutil.LabelValue(build.Name)}, actual.Labels) {
 		t.Errorf("Pod Labels does not match Build Labels!")
 	}
 	if !reflect.DeepEqual(nodeSelector, actual.Spec.NodeSelector) {
@@ -182,8 +181,8 @@ func TestCustomBuildLongName(t *testing.T) {
 	if err != nil {
 		t.Fatalf("unexpected: %v", err)
 	}
-	if pod.Labels[util.BuildLabel] != build.Name[:validation.DNS1123LabelMaxLength] {
-		t.Errorf("Unexpected build label value: %s", pod.Labels[util.BuildLabel])
+	if pod.Labels[buildutil.BuildLabel] != build.Name[:validation.DNS1123LabelMaxLength] {
+		t.Errorf("Unexpected build label value: %s", pod.Labels[buildutil.BuildLabel])
 	}
 }
 

pkg/build/controller/strategy/docker_test.go
@@ -14,8 +14,7 @@ import (
 	kapihelper "k8s.io/kubernetes/pkg/apis/core/helper"
 
 	buildv1 "github.com/openshift/api/build/v1"
-	"github.com/openshift/origin/pkg/build/buildapihelpers"
-	buildutil "github.com/openshift/origin/pkg/build/util"
+	buildutil "github.com/openshift/origin/pkg/build/buildutil"
 )
 
 func TestDockerCreateBuildPod(t *testing.T) {
@@ -29,10 +28,10 @@ func TestDockerCreateBuildPod(t *testing.T) {
 		t.Errorf("Unexpected error: %v", err)
 	}
 
-	if expected, actual := buildapihelpers.GetBuildPodName(build), actual.ObjectMeta.Name; expected != actual {
+	if expected, actual := buildutil.GetBuildPodName(build), actual.ObjectMeta.Name; expected != actual {
 		t.Errorf("Expected %s, but got %s!", expected, actual)
 	}
-	if !reflect.DeepEqual(map[string]string{buildutil.BuildLabel: buildapihelpers.LabelValue(build.Name)}, actual.Labels) {
+	if !reflect.DeepEqual(map[string]string{buildutil.BuildLabel: buildutil.LabelValue(build.Name)}, actual.Labels) {
 		t.Errorf("Pod Labels does not match Build Labels!")
 	}
 	if !reflect.DeepEqual(nodeSelector, actual.Spec.NodeSelector) {

pkg/build/controller/strategy/sti_test.go
@@ -19,8 +19,7 @@ import (
 	securityv1 "github.com/openshift/api/security/v1"
 	securityv1typed "github.com/openshift/client-go/security/clientset/versioned/typed/security/v1"
 	securityv1fake "github.com/openshift/client-go/security/clientset/versioned/typed/security/v1/fake"
-	"github.com/openshift/origin/pkg/build/buildapihelpers"
-	buildutil "github.com/openshift/origin/pkg/build/util"
+	buildutil "github.com/openshift/origin/pkg/build/buildutil"
 )
 
 func newFakeSecurityClient(rootAllowed bool) securityv1typed.SecurityV1Interface {
@@ -62,10 +61,10 @@ func testSTICreateBuildPod(t *testing.T, rootAllowed bool) {
 		t.Errorf("Unexpected error: %v", err)
 	}
 
-	if expected, actual := buildapihelpers.GetBuildPodName(build), actual.ObjectMeta.Name; expected != actual {
+	if expected, actual := buildutil.GetBuildPodName(build), actual.ObjectMeta.Name; expected != actual {
 		t.Errorf("Expected %s, but got %s!", expected, actual)
 	}
-	if !reflect.DeepEqual(map[string]string{buildutil.BuildLabel: buildapihelpers.LabelValue(build.Name)}, actual.Labels) {
+	if !reflect.DeepEqual(map[string]string{buildutil.BuildLabel: buildutil.LabelValue(build.Name)}, actual.Labels) {
 		t.Errorf("Pod Labels does not match Build Labels!")
 	}
 	if !reflect.DeepEqual(nodeSelector, actual.Spec.NodeSelector) {

pkg/build/controller/strategy/util.go
@@ -17,8 +17,7 @@ import (
 	buildv1 "github.com/openshift/api/build/v1"
 	"github.com/openshift/library-go/pkg/build/naming"
 	"github.com/openshift/library-go/pkg/image/reference"
-	"github.com/openshift/origin/pkg/build/buildapihelpers"
-	buildutil "github.com/openshift/origin/pkg/build/util"
+	buildutil "github.com/openshift/origin/pkg/build/buildutil"
 )
 
 const (
@@ -299,7 +298,7 @@ func setupAdditionalSecrets(pod *corev1.Pod, container *corev1.Container, secret
 
 // getPodLabels creates labels for the Build Pod
 func getPodLabels(build *buildv1.Build) map[string]string {
-	return map[string]string{buildutil.BuildLabel: buildapihelpers.LabelValue(build.Name)}
+	return map[string]string{buildutil.BuildLabel: buildutil.LabelValue(build.Name)}
 }
 
 func makeOwnerReference(build *buildv1.Build) metav1.OwnerReference {
@@ -353,7 +352,7 @@ func setupContainersConfigs(build *buildv1.Build, pod *corev1.Pod) {
 	if !exists {
 		cmSource := &corev1.ConfigMapVolumeSource{
 			LocalObjectReference: corev1.LocalObjectReference{
-				Name: buildapihelpers.GetBuildSystemConfigMapName(build),
+				Name: buildutil.GetBuildSystemConfigMapName(build),
 			},
 		}
 		pod.Spec.Volumes = append(pod.Spec.Volumes,
@@ -492,7 +491,7 @@ func setupBuildCAs(build *buildv1.Build, pod *corev1.Pod, additionalCAs map[stri
 		// See https://github.com/openshift/service-serving-cert-signer
 		cmSource := &corev1.ConfigMapVolumeSource{
 			LocalObjectReference: corev1.LocalObjectReference{
-				Name: buildapihelpers.GetBuildCAConfigMapName(build),
+				Name: buildutil.GetBuildCAConfigMapName(build),
 			},
 			Items: []corev1.KeyToPath{
 				{

openshift/openshift-controller-manager

pkg/build/controller/strategy/custom_test.go
@@ -15,7 +15,7 @@ import (
 	kapihelper "k8s.io/kubernetes/pkg/apis/core/helper"
 
 	buildv1 "github.com/openshift/api/build/v1"
-	"github.com/openshift/origin/pkg/build/buildapihelpers"
+	"github.com/openshift/origin/pkg/build/buildutil"
 )
 
 func TestCustomCreateBuildPod(t *testing.T) {
@@ -36,10 +36,10 @@ func TestCustomCreateBuildPod(t *testing.T) {
 		t.Fatalf("Unexpected error: %v", err)
 	}
 
-	if expected, actual := buildapihelpers.GetBuildPodName(build), actual.ObjectMeta.Name; expected != actual {
+	if expected, actual := buildutil.GetBuildPodName(build), actual.ObjectMeta.Name; expected != actual {
 		t.Errorf("Expected %s, but got %s!", expected, actual)
 	}
-	if !reflect.DeepEqual(map[string]string{buildv1.BuildLabel: buildapihelpers.LabelValue(build.Name)}, actual.Labels) {
+	if !reflect.DeepEqual(map[string]string{buildv1.BuildLabel: buildutil.LabelValue(build.Name)}, actual.Labels) {
 		t.Errorf("Pod Labels does not match Build Labels!")
 	}
 	if !reflect.DeepEqual(nodeSelector, actual.Spec.NodeSelector) {

pkg/build/controller/strategy/docker_test.go
@@ -14,8 +14,7 @@ import (
 	kapihelper "k8s.io/kubernetes/pkg/apis/core/helper"
 
 	buildv1 "github.com/openshift/api/build/v1"
-	"github.com/openshift/origin/pkg/build/buildapihelpers"
-	buildutil "github.com/openshift/origin/pkg/build/util"
+	buildutil "github.com/openshift/origin/pkg/build/buildutil"
 )
 
 func TestDockerCreateBuildPod(t *testing.T) {
@@ -29,10 +28,10 @@ func TestDockerCreateBuildPod(t *testing.T) {
 		t.Errorf("Unexpected error: %v", err)
 	}
 
-	if expected, actual := buildapihelpers.GetBuildPodName(build), actual.ObjectMeta.Name; expected != actual {
+	if expected, actual := buildutil.GetBuildPodName(build), actual.ObjectMeta.Name; expected != actual {
 		t.Errorf("Expected %s, but got %s!", expected, actual)
 	}
-	if !reflect.DeepEqual(map[string]string{buildv1.BuildLabel: buildapihelpers.LabelValue(build.Name)}, actual.Labels) {
+	if !reflect.DeepEqual(map[string]string{buildv1.BuildLabel: buildutil.LabelValue(build.Name)}, actual.Labels) {
 		t.Errorf("Pod Labels does not match Build Labels!")
 	}
 	if !reflect.DeepEqual(nodeSelector, actual.Spec.NodeSelector) {

pkg/build/controller/strategy/sti_test.go
@@ -19,8 +19,7 @@ import (
 	securityv1 "github.com/openshift/api/security/v1"
 	securityv1typed "github.com/openshift/client-go/security/clientset/versioned/typed/security/v1"
 	securityv1fake "github.com/openshift/client-go/security/clientset/versioned/typed/security/v1/fake"
-	"github.com/openshift/origin/pkg/build/buildapihelpers"
-	buildutil "github.com/openshift/origin/pkg/build/util"
+	buildutil "github.com/openshift/origin/pkg/build/buildutil"
 )
 
 func newFakeSecurityClient(rootAllowed bool) securityv1typed.SecurityV1Interface {
@@ -62,10 +61,10 @@ func testSTICreateBuildPod(t *testing.T, rootAllowed bool) {
 		t.Errorf("Unexpected error: %v", err)
 	}
 
-	if expected, actual := buildapihelpers.GetBuildPodName(build), actual.ObjectMeta.Name; expected != actual {
+	if expected, actual := buildutil.GetBuildPodName(build), actual.ObjectMeta.Name; expected != actual {
 		t.Errorf("Expected %s, but got %s!", expected, actual)
 	}
-	if !reflect.DeepEqual(map[string]string{buildv1.BuildLabel: buildapihelpers.LabelValue(build.Name)}, actual.Labels) {
+	if !reflect.DeepEqual(map[string]string{buildv1.BuildLabel: buildutil.LabelValue(build.Name)}, actual.Labels) {
 		t.Errorf("Pod Labels does not match Build Labels!")
 	}
 	if !reflect.DeepEqual(nodeSelector, actual.Spec.NodeSelector) {

pkg/build/controller/strategy/util.go
@@ -17,8 +17,7 @@ import (
 	buildv1 "github.com/openshift/api/build/v1"
 	"github.com/openshift/library-go/pkg/build/naming"
 	"github.com/openshift/library-go/pkg/image/reference"
-	"github.com/openshift/origin/pkg/build/buildapihelpers"
-	buildutil "github.com/openshift/origin/pkg/build/util"
+	buildutil "github.com/openshift/origin/pkg/build/buildutil"
 )
 
 const (
@@ -299,7 +298,7 @@ func setupAdditionalSecrets(pod *corev1.Pod, container *corev1.Container, secret
 
 // getPodLabels creates labels for the Build Pod
 func getPodLabels(build *buildv1.Build) map[string]string {
-	return map[string]string{buildv1.BuildLabel: buildapihelpers.LabelValue(build.Name)}
+	return map[string]string{buildv1.BuildLabel: buildutil.LabelValue(build.Name)}
 }
 
 func makeOwnerReference(build *buildv1.Build) metav1.OwnerReference {
@@ -353,7 +352,7 @@ func setupContainersConfigs(build *buildv1.Build, pod *corev1.Pod) {
 	if !exists {
 		cmSource := &corev1.ConfigMapVolumeSource{
 			LocalObjectReference: corev1.LocalObjectReference{
-				Name: buildapihelpers.GetBuildSystemConfigMapName(build),
+				Name: buildutil.GetBuildSystemConfigMapName(build),
 			},
 		}
 		pod.Spec.Volumes = append(pod.Spec.Volumes,
@@ -492,7 +491,7 @@ func setupBuildCAs(build *buildv1.Build, pod *corev1.Pod, additionalCAs map[stri
 		// See https://github.com/openshift/service-serving-cert-signer
 		cmSource := &corev1.ConfigMapVolumeSource{
 			LocalObjectReference: corev1.LocalObjectReference{
-				Name: buildapihelpers.GetBuildCAConfigMapName(build),
+				Name: buildutil.GetBuildCAConfigMapName(build),
 			},
 			Items: []corev1.KeyToPath{
 				{