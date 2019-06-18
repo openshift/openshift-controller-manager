openshift/origin

fallback to the default builder SA secret for build pushes/pulls

pkg/build/controller/build/build_controller.go
@@ -516,12 +516,17 @@ func (bc *BuildController) resolveImageSecretAsReference(build *buildapi.Build, 
 	if err != nil {
 		return nil, fmt.Errorf("Error getting push/pull secrets for service account %s/%s: %v", build.Namespace, serviceAccount, err)
 	}
-	secret := buildutil.FindDockerSecretAsReference(builderSecrets, imagename)
+	var secret *kapi.LocalObjectReference
+	if len(imagename) != 0 {
+		secret = buildutil.FindDockerSecretAsReference(builderSecrets, imagename)
+	}
 	if secret == nil {
+		glog.V(4).Infof("build %s is referencing an unknown image, will attempt to use the default secret for the service account", build.Name)
 		dockerSecretExists := false
 		for _, builderSecret := range builderSecrets {
 			if builderSecret.Type == kapi.SecretTypeDockercfg || builderSecret.Type == kapi.SecretTypeDockerConfigJson {
 				dockerSecretExists = true
+				secret = &kapi.LocalObjectReference{Name: builderSecret.Name}
 				break
 			}
 		}
@@ -530,7 +535,7 @@ func (bc *BuildController) resolveImageSecretAsReference(build *buildapi.Build, 
 		if !dockerSecretExists {
 			return nil, fmt.Errorf("No docker secrets associated with build service account %s", serviceAccount)
 		}
-		glog.V(4).Infof("No secrets found for pushing or pulling image named %s for build %s/%s", imagename, build.Namespace, build.Name)
+		glog.V(4).Infof("No secrets found for pushing or pulling image named %s for build, using default: %s %s/%s", imagename, build.Namespace, build.Name, secret.Name)
 	}
 	return secret, nil
 }
@@ -822,9 +827,11 @@ func (bc *BuildController) createBuildPod(build *buildapi.Build) (*buildUpdate, 
 		pullSecret = build.Spec.Strategy.CustomStrategy.PullSecret
 		imageName = build.Spec.Strategy.CustomStrategy.From.Name
 	}
-	// Only look up a pull secret if the user hasn't explicitly provided one and
-	// we have a base/builder image (Docker builds may not have one).
-	if pullSecret == nil && len(imageName) != 0 {
+
+	// Only look up a pull secret if the user hasn't explicitly provided one
+	// if we don't know what image they are referencing, we'll end up using the
+	// docker secret associated w/ the build's service account.
+	if pullSecret == nil {
 		var err error
 		pullSecret, err = bc.resolveImageSecretAsReference(build, imageName)
 		if err != nil {

pkg/build/controller/build/build_controller_test.go
@@ -1050,10 +1050,24 @@ func newFakeBuildController(buildClient buildinternalclientset.Interface, imageC
 		kubeExternalClient = fakeKubeExternalClientSet()
 	}
 	if kubeInternalClient == nil {
-		builderSA := kapi.ServiceAccount{}
+		builderSA := &kapi.ServiceAccount{}
 		builderSA.Name = "builder"
 		builderSA.Namespace = "namespace"
-		kubeInternalClient = fakeKubeInternalClientSet(&builderSA)
+		builderSA.Secrets = []kapi.ObjectReference{
+			{
+				Name: "secret",
+			},
+		}
+
+		secret := &kapi.Secret{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "secret",
+				Namespace: "namespace",
+			},
+			Type: kapi.SecretTypeDockerConfigJson,
+		}
+
+		kubeInternalClient = fakeKubeInternalClientSet(builderSA, secret)
 	}
 
 	kubeExternalInformers := fakeKubeExternalInformers(kubeExternalClient)

openshift/openshift-controller-manager

pkg/build/controller/build/build_controller.go
@@ -543,12 +543,17 @@ func (bc *BuildController) resolveImageSecretAsReference(build *buildv1.Build, i
 	if err != nil {
 		return nil, fmt.Errorf("Error getting push/pull secrets for service account %s/%s: %v", build.Namespace, serviceAccount, err)
 	}
-	secret := buildutil.FindDockerSecretAsReference(builderSecrets, imagename)
+	var secret *corev1.LocalObjectReference
+	if len(imagename) != 0 {
+		secret = buildutil.FindDockerSecretAsReference(builderSecrets, imagename)
+	}
 	if secret == nil {
+		glog.V(4).Infof("build %s is referencing an unknown image, will attempt to use the default secret for the service account", build.Name)
 		dockerSecretExists := false
 		for _, builderSecret := range builderSecrets {
 			if builderSecret.Type == corev1.SecretTypeDockercfg || builderSecret.Type == corev1.SecretTypeDockerConfigJson {
 				dockerSecretExists = true
+				secret = &corev1.LocalObjectReference{Name: builderSecret.Name}
 				break
 			}
 		}
@@ -557,7 +562,7 @@ func (bc *BuildController) resolveImageSecretAsReference(build *buildv1.Build, i
 		if !dockerSecretExists {
 			return nil, fmt.Errorf("No docker secrets associated with build service account %s", serviceAccount)
 		}
-		glog.V(4).Infof("No secrets found for pushing or pulling image named %s for build %s/%s", imagename, build.Namespace, build.Name)
+		glog.V(4).Infof("No secrets found for pushing or pulling image named %s for build, using default: %s %s/%s", imagename, build.Namespace, build.Name, secret.Name)
 	}
 	return secret, nil
 }
@@ -849,9 +854,11 @@ func (bc *BuildController) createBuildPod(build *buildv1.Build) (*buildUpdate, e
 		pullSecret = build.Spec.Strategy.CustomStrategy.PullSecret
 		imageName = build.Spec.Strategy.CustomStrategy.From.Name
 	}
-	// Only look up a pull secret if the user hasn't explicitly provided one and
-	// we have a base/builder image (Docker builds may not have one).
-	if pullSecret == nil && len(imageName) != 0 {
+
+	// Only look up a pull secret if the user hasn't explicitly provided one
+	// if we don't know what image they are referencing, we'll end up using the
+	// docker secret associated w/ the build's service account.
+	if pullSecret == nil {
 		var err error
 		pullSecret, err = bc.resolveImageSecretAsReference(build, imageName)
 		if err != nil {

pkg/build/controller/build/build_controller_test.go
@@ -1256,7 +1256,23 @@ func fakeBuildClient(objects ...runtime.Object) buildv1client.Interface {
 }
 
 func fakeKubeExternalClientSet(objects ...runtime.Object) kubernetes.Interface {
-	return fake.NewSimpleClientset(objects...)
+	builderSA := &corev1.ServiceAccount{}
+	builderSA.Name = "builder"
+	builderSA.Namespace = "namespace"
+	builderSA.Secrets = []corev1.ObjectReference{
+		{
+			Name: "secret",
+		},
+	}
+
+	secret := &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "secret",
+			Namespace: "namespace",
+		},
+		Type: corev1.SecretTypeDockerConfigJson,
+	}
+	return fake.NewSimpleClientset(append(objects, builderSA, secret)...)
 }
 
 func fakeKubeInternalClientSet(objects ...runtime.Object) kubernetes.Interface {
@@ -1297,12 +1313,6 @@ func newFakeBuildController(buildClient buildv1client.Interface, imageClient ima
 	}
 	if kubeExternalClient == nil {
 		kubeExternalClient = fakeKubeExternalClientSet()
-	}
-	if kubeInternalClient == nil {
-		builderSA := corev1.ServiceAccount{}
-		builderSA.Name = "builder"
-		builderSA.Namespace = "namespace"
-		kubeInternalClient = fakeKubeInternalClientSet(&builderSA)
 	}
 
 	kubeExternalInformers := fakeKubeExternalInformers(kubeExternalClient)