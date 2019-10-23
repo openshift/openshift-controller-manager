0a9bec31fd898822eee4d1604634f3d2afbbd646
Adam Kaplan <adam.kaplan@redhat.com>
2019-10-23 10:10:33 -0400

    add dependency magnet for go mod

---
+++ make/doc.go
@@ -0,0 +1,7 @@
+package make
+
+import (
+	// alpha-build-machinery has non-go code needed for makefiles.
+	// Needed to work with go modules.
+	_ "github.com/openshift/library-go/alpha-build-machinery"
+)
